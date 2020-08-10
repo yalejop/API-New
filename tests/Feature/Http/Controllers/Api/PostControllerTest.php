b37836a6976ff6a6827502d6d29f129558e7279c
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-08-10 13:01:13 -0500

    feat: create test in store

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -4,14 +4,24 @@ namespace Tests\Feature\Http\Controllers\Api;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use phpDocumentor\Reflection\Types\This;
 use Tests\TestCase;
 
 class PostControllerTest extends TestCase
 {
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_store()
+    {
+        //$this->withoutExceptionHandling();
+        $response = $this->json('POST', 'api/posts', [
+            'title' => 'El post de prueba'
+        ]);
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => 'El post de prueba'])
+            ->assertStatus(201); //ok, creado un recurso
+
+        $this->assertDatabaseHas('posts', ['title' => 'El post de prueba']);
     }
 }
